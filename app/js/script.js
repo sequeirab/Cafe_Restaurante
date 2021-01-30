5a154b55127c8e7e87b27672421a210c8a5410c8
Bruno Sequeira <sequeirab86@gmail.com>
2021-01-30 22:41:06 +0000

    finishing functionality and responsiveness of the nav bar

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,8 @@
-console.log('hello world');
+const menu = document.getElementById('hamburgerMenu');
+
+
+menu.addEventListener('click', () => {
+    const showMenu = document.querySelector('.menu-block');
+    console.log('works')
+    showMenu.classList.toggle('hide');
+})
