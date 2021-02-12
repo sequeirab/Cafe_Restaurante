40007900520abaa7accc8e5cd1bc8f4f7f8a1103
Bruno Sequeira <sequeirab86@gmail.com>
2021-02-12 22:41:18 +0000

    major problems dots slide image

--- app/js/script.js
+++ app/js/script.js
@@ -1,6 +1,9 @@
 const menu = document.getElementById('hamburgerMenu');
 const slide = document.querySelector("#slide");
 const numText =document.querySelector(".hero__numberText");
+const dots = document.querySelector(".hero__dots");
+const dot = document.querySelector(".dot");
+let elmArr = [];
 let images = [];
 let time = 5000;
 let i = 0;
@@ -18,15 +21,43 @@ menu.addEventListener('click', () => {
 images[0] = './images/slide1.jfif';
 images[1] = './images/slide2.png';
 images[2] = './images/slide3.png';
+images[3] = './images/slide2.png';
+images[4] = './images/slide3.png';
+images[5] = './images/slide2.png';
+images[6] = './images/slide3.png';
+
+for(let j = 0; j < images.length; j++) {
+    const newEl = document.createElement("div");
+    newEl.classList.add("dot");
+    dots.appendChild(newEl);
+    elmArr.push(newEl);
+}
+//Function to change dots
+
+const changeDots = () => {
+    let k = 0;
+    if(k = 0){
+        elmArr[k].classList.add("active");
+    } else if(k > 0) {
+        elmArr[k].classList.add("active");
+        elmArr[k - 1].classList.remove("active");
+    }
+    k++;
+    console.log("executando", k);
+}
+
 
 
 //Function to change image
 
 const changeImg = () => {
+    changeDots();
+    
     slide.src = images[i];
     numText.innerHTML = `${i + 1} of ${images.length}`;
     
     i++;   
+    
     
     if(i < images.length) {
         setTimeout(changeImg,  time);
